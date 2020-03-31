e69b50009b6a703098ef9018411bdc5113f0e85c
YangJ0605 <1442122744@qq.com>
2020-03-31 17:25:01 +0800

    build

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 //顶部进度条
 import NProgress from 'nprogress'
 // http://server.sineava.top/api/private/v1/
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://47.105.143.52:8888/api/private/v1/' : 'http://127.0.0.1:8888/api/private/v1/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://ccc/api/private/v1/' : 'http://127.0.0.1:8888/api/private/v1/'
 export function request(config) {
  const instance = axios.create({
    baseURL,
